c3a791ff68c96b3b18cbc8ff5379ddff48ef42f5
Rogério <rogerio.alves651@gmail.com>
2024-12-22 21:50:04 -0300

    Pastaversao_antiga_react-native adicionada.

--- versao_antiga_react-native/projeto-props/src/App.js
+++
@@ -1,9 +0,0 @@
-function Greeting(props) {
-  return <h1 style={{color: "orange", textAlign: "center"}}>Olá, {props.name}!</h1>;
-}
-
-function App() {
-  return <Greeting name="Júlia" />;
-}
-
-export default App;
