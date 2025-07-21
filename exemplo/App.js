7e3a8b171afa6734ec8af4f87079f0d4bbb954be
Rogerio <rogerio.alves651@gmail.com>
2025-07-21 01:22:48 -0300

    Alterar a cor de fundo para rosa e alinhar itens à esquerda no componente App.

--- exemplo/App.js
+++ exemplo/App.js
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'pink',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
